8a4228613053803538e11e3abe0a412d555d0342
wnandirta <nandira11@gmail.com>
2021-10-13 03:00:03 +0700

    Property Padding

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Margin")),
+          appBar: AppBar(title: Text("Contoh Padding")),
           body: Container(
-              margin: EdgeInsets.all(50),
+              padding: EdgeInsets.only(left: 20),
+              margin: EdgeInsets.all(20),
               height: 200,
               width: 200,
               alignment: Alignment.topLeft,
               color: Colors.blueGrey,
               child: Text(
-                'Semangat Belajar Flutter',
+                'Ayo Belajar Flutter',
                 style: TextStyle(fontSize: 20, color: Colors.white),
               ))),
     );
