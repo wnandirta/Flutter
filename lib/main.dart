d9d6697e3aaf8b1269f97a5d6e7d3389ad4c8e22
wnandirta <nandira11@gmail.com>
2021-10-13 02:46:07 +0700

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your onPressed code here!
-          },
-          child: Icon(Icons.thumb_up),
-          backgroundColor: Colors.pink,
+        appBar: AppBar(
+          title: Text('Sample Code'),
         ),
+        body: Center(
+          child: Text('You have pressed the button $_count times.'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 50.0,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => 0,
+          tooltip: 'Increment Counter',
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
