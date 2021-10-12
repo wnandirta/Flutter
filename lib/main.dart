fa18e8f781b27e6f89bd23d232a2092d02813573
wnandirta <nandira11@gmail.com>
2021-10-13 02:44:50 +0700

    button

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        margin: EdgeInsets.only(top: 30),
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            AppBar(title: Text('Contoh Cupertino')),
-            CupertinoButton(
-              child: Text("Contoh button"),
-              onPressed: () {},
-            ),
-            CupertinoActivityIndicator(),
-          ],
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
         ),
       ),
     );
