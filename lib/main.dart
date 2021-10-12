d00f81c3f1d40229feb900c6daee5d3547fcb9db
wnandirta <nandira11@gmail.com>
2021-10-13 03:11:25 +0700

    Stack

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Row Widget")),
-        body: Row(
-          children: [
+        appBar: AppBar(
+          title: Text("Contoh Stack Widget"),
+        ),
+        body: Stack(
+          children: <Widget>[
             Container(
-              color: Colors.purpleAccent,
-              child: FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.green,
+              alignment: Alignment.bottomCenter,
+              child: Text("Satu",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
             ),
             Container(
-              color: Colors.greenAccent,
-              child: FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.red,
+              alignment: Alignment.bottomCenter,
+              child: Text("Dua",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 400.0,
+              width: 300.0,
             ),
             Container(
-              color: Colors.orangeAccent,
-              child: FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.deepPurple,
+              alignment: Alignment.bottomCenter,
+              child: Text("Tiga",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 200.0,
+              width: 200.0,
             ),
           ],
         ),
