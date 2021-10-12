e28dd61081e1ff73b4c27846e317faa2bf2b35e1
wnandirta <nandira11@gmail.com>
2021-10-13 02:23:00 +0700

    Text Widget

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('This is Text Widget,'),
+    );
+  }
+}
