8b799d419ff94670040606411219840cc7c8910b
wnandirta <nandira11@gmail.com>
2021-10-13 02:55:57 +0700

    Property Color

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Color")),
+          appBar: AppBar(title: Text("Contoh Height dan Width")),
           body: Container(
               margin: EdgeInsets.all(50),
               height: 200,
               width: 200,
               alignment: Alignment.center,
-              color: Colors.amber[900],
+              color: Colors.blueGrey,
               child: Text(
                 'Semangat Belajar',
                 style: TextStyle(fontSize: 20, color: Colors.white),
