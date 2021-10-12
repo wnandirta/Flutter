a6bcc38ec91bd0ac8c5be92dd5acb4f162141613
wnandirta <nandira11@gmail.com>
2021-10-13 02:52:50 +0700

    property child

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,15 +8,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh TextField")),
-        body: TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
-          ),
-        ),
-      ),
+          appBar: AppBar(title: Text("Contoh Button")),
+          body: Container(
+              child: RaisedButton(
+            textColor: Colors.white,
+            onPressed: () {},
+            color: Colors.red[900],
+            child: Text("Button", style: TextStyle(fontSize: 20)),
+          ))),
     );
   }
 }
