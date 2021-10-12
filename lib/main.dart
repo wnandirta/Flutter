2af1c7dc62e97232f98c830a30ca0d1a33cb947e
wnandirta <nandira11@gmail.com>
2021-10-13 02:59:20 +0700

    Property Margin

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Height dan Width")),
+          appBar: AppBar(title: Text("Contoh Margin")),
           body: Container(
               margin: EdgeInsets.all(50),
               height: 200,
               width: 200,
-              alignment: Alignment.center,
+              alignment: Alignment.topLeft,
               color: Colors.blueGrey,
               child: Text(
-                'Semangat Belajar',
+                'Semangat Belajar Flutter',
                 style: TextStyle(fontSize: 20, color: Colors.white),
               ))),
     );
