5d610f9e3ec238175612e8d5cbb80fbc389aeb27
wnandirta <nandira11@gmail.com>
2021-10-13 02:54:07 +0700

    Property Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Button")),
+          appBar: AppBar(title: Text("Contoh Aligment")),
           body: Container(
-              child: RaisedButton(
-            textColor: Colors.white,
-            onPressed: () {},
-            color: Colors.red[900],
-            child: Text("Button", style: TextStyle(fontSize: 20)),
-          ))),
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Semangat Belajar',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ))),
     );
   }
 }
