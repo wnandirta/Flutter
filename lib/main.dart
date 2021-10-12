a48bc079d618c32398a1a92b1fb7b5fc7bd470b5
wnandirta <nandira11@gmail.com>
2021-10-13 02:30:58 +0700

    Cupertino WIdget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Image(
-        image: NetworkImage(
-            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+      home: Container(
+        margin: EdgeInsets.only(top: 30),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            AppBar(title: Text('Contoh Cupertino')),
+            CupertinoButton(
+              child: Text("Contoh button"),
+              onPressed: () {},
+            ),
+            CupertinoActivityIndicator(),
+          ],
+        ),
       ),
     );
   }
